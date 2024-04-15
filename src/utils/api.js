58c78ed15eb6e64746cc205ba23217dce6ced46a
Alexana7 <ngayane@bk.ru>
2024-04-15 09:48:40 +0300

    add new token

--- src/utils/api.js
+++ src/utils/api.js
@@ -89,7 +89,7 @@ const api = new Api ({
     baseUrl: 'https://api.react-learning.ru/v2/group-11',
     headers: {
         'content-type': 'application/json',
-        authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEwN2UwOGFhMzk3MTIxODM4ZjI4Y2MiLCJncm91cCI6Imdyb3VwLTExIiwiaWF0IjoxNjc4ODAyNDQ1LCJleHAiOjE3MTAzMzg0NDV9.kZEWWebomQEcFQ3JyjUuF8l3B_o5sLselfMazIwH6VM'
+        authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjFjMzI4YWVhNjJiZjA0MGJlMzA3MjAiLCJncm91cCI6Imdyb3VwLTExIiwiaWF0IjoxNzEzMTI0MTMyLCJleHAiOjE3NDQ2NjAxMzJ9.c4D4wMuukXcQ3PHWnc4x5n_tIQ4pIEO6Eez2YUKJqTM'
     }
 }
 )
